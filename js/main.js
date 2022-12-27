b230a294432f597c3742bb362ded5ec45409d41a
shevchenkoak <demorest49de@gmail.com>
2022-12-27 21:39:00 +0300

    main.js h2 changed;

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 document.querySelector('.ads').remove();
-const itemTwoList = document.querySelector('.item_two .props__list');
+const itemTwo = document.querySelector('.item_two .props__list');
 const itemThree = document.querySelector('.item_three');
 const itemFour = document.querySelector('.item_four');
 const itemFive = document.querySelector('.item_five');
@@ -15,10 +15,20 @@ const propsItemsFive = itemFive.querySelector('.props__list');
 
 const propsItemsTwo = itemSix.querySelectorAll('.props__item_two');
 
+const h2ItemTwo = document.querySelector('.item_two .item__title');
+
+const h2ItemFive = document.querySelector('.item_five .item__title');
+
+const h2ItemSix = document.querySelector('.item_six .item__title');
+
 itemsAll[3].after(itemFour);
 propsItemsFour[2].after(propItemFour);
 
 propsItemsThree.replaceWith(propsItemsFive);
 itemFive.querySelector('.content').append(propsItemsThree);
-itemTwoList.after(propsItemsTwo[0]);
-itemTwoList.after(propsItemsTwo[1]);
+itemTwo.after(propsItemsTwo[0]);
+itemTwo.after(propsItemsTwo[1]);
+
+h2ItemTwo.replaceWith(h2ItemFive);
+h2ItemSix.replaceWith(h2ItemTwo);
+propsItemsThree.before(h2ItemSix)
