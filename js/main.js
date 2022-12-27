773472105d1827eb0b998523b5c37ce1a3557f4d
shevchenkoak <demorest49de@gmail.com>
2022-12-27 17:46:15 +0300

    main.js changed;

--- js/main.js
+++ js/main.js
@@ -5,20 +5,20 @@ const itemThree = document.querySelector('.item_three');
 const itemFour = document.querySelector('.item_four');
 const itemFive = document.querySelector('.item_five');
 const itemSix = document.querySelector('.item_six');
-const items = document.querySelectorAll('.item');
-items[3].after(itemFour);
+const itemsAll = document.querySelectorAll('.item');
 
 const propItemFour = document.querySelector('.item_two .props__item_four');
 const propsItemsFour = itemFour.querySelectorAll('.props__item_four');
 
-propsItemsFour[2].after(propItemFour);
-
 const propsItemsThree = itemThree.querySelector('.props__list');
 const propsItemsFive = itemFive.querySelector('.props__list');
 
+const propsItemsTwo = itemSix.querySelectorAll('.props__item_two');
+
+itemsAll[3].after(itemFour);
+propsItemsFour[2].after(propItemFour);
+
 propsItemsThree.replaceWith(propsItemsFive);
 itemFive.querySelector('.content').append(propsItemsThree);
-
-const propsItemsTwo = itemSix.querySelectorAll('.props__item_two');
 itemTwoList.after(propsItemsTwo[0]);
 itemTwoList.after(propsItemsTwo[1]);
