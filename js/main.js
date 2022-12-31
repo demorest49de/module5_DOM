912b09ddea24b13a973c177044cc0843a3ca4c54
shevchenkoak <demorest49de@gmail.com>
2022-12-31 21:07:35 +0300

    additem.css changed; basic.css class added; index.html changed; main.js changed;

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const addItemTitle = document.querySelector('.add-item__title');
+const addItemEditBtn = document.querySelector('.add-item__edit-btn');
+const addItemIdNumber = document.querySelector('.add-item__id-number');
+const addItemForm = document.querySelector('#add-item__form');
+const addItemCheckbox = document.querySelector('.add-item__checkbox');
+const addItemDiscount = document.querySelector('#add-item__discount');
+const addItemTotalValue = document.querySelector('.add-item__total-value');
+
+console.log('addItemTitle: ', addItemTitle);
+console.log('addItemEditBtn: ', addItemEditBtn);
+console.log('addItemIdNumber: ', addItemIdNumber);
+console.log('addItemForm: ', addItemForm);
+console.log('addItemCheckbox: ', addItemCheckbox);
+console.log('addItemDiscount: ', addItemDiscount);
+console.log('addItemTotalValue: ', addItemTotalValue);
